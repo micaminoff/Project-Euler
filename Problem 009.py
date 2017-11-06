68dc23a98a0ef3ba5db13dc482af33b73df3f952
Michael Aminoff <micaminoff@gmail.com>
2017-11-06 16:11:44 +0200

    Made 009 O(n^2), solved 029

--- Problem 009.py
+++ Problem 009.py
@@ -2,24 +2,24 @@
 # a^2 + b^2 = c^2. For example 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
-# 27s
-# O(n^3), Horrendous performance. Needs to be improved
+# 0.15s
+# O(n^2), bad performance. Could be improved
 
 import time
-
 start_time = time.time()
 
 
 def is_triplet(a, b, c):
     return a ** 2 + b ** 2 == c ** 2        # Return true if the condition is satisfied
 
-for x in range(1, 1000):
-    # test a
-    for y in range(x, 1000 - x):
-        # test b
-        for z in range(y, 1000 - y):
-            # test c
-            if is_triplet(x, y, z) and x + y + z == 1000:
-                print(x * y * z)
-                print(time.time() - start_time)
-                exit()
+
+def find_pythagorean_triplet(sum_to_find):
+    for a in range(1, sum_to_find):
+        for b in range(a, sum_to_find):     # a and b are interchangeable so no need to check all values of b
+            c = sum_to_find - a - b         # a+b+c has to equal sum_to_find
+            if is_triplet(a, b, c):
+                return a, b, c
+
+triplet = find_pythagorean_triplet(1000)
+print(triplet[0]*triplet[1]*triplet[2])     # Calculate and output product of triplet
+print(time.time() - start_time)
