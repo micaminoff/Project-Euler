382fdd8a58fbc79412fd4e37e8c9b7e01740ae05
Michael Aminoff <micaminoff@gmail.com>
2017-11-06 19:21:51 +0200

    Slight improvement to 009

--- Problem 009.py
+++ Problem 009.py
@@ -2,7 +2,7 @@
 # a^2 + b^2 = c^2. For example 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
-# 0.15s
+# 0.13s
 # O(n^2), bad performance. Could be improved
 
 import time
@@ -15,8 +15,8 @@ def is_triplet(a, b, c):
 
 def find_pythagorean_triplet(sum_to_find):
     for a in range(1, sum_to_find):
-        for b in range(a, sum_to_find):     # a and b are interchangeable so no need to check all values of b
-            c = sum_to_find - a - b         # a+b+c has to equal sum_to_find
+        for b in range(a, sum_to_find - a):  # a and b are interchangeable so no need to check all values of b
+            c = sum_to_find - a - b          # a+b+c has to equal sum_to_find
             if is_triplet(a, b, c):
                 return a, b, c
 
