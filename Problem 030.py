950adc237a110825d54907163a39676a962cb313
Michael Aminoff <michael@noviaria.com>
2017-12-09 17:53:41 +0200

    Commented code for 30

--- Problem 030.py
+++ Problem 030.py
@@ -6,18 +6,18 @@
 #
 # The sum of these numbers is 1634 + 8208 + 9474 = 19316.
 # Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
 import time
 start_time = time.time()
 
 digit_fifth_powers = []
 
 for i in range(10000, 1000000000):
-    s = 0
-    for digit in str(i):
-        s += int(digit)**5
+    s = 0                           # variable for summing fifth power of each digit
+    for digit in str(i):            # for each digit in i
+        s += int(digit)**5          # add digit^5 to the sum
     if s == i:
-        digit_fifth_powers.append(i)
+        digit_fifth_powers.append(i)  # add the number
 
-print(digit_fifth_powers)
 print(sum(digit_fifth_powers))
 print(time.time() - start_time)
